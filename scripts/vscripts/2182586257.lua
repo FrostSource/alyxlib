535a810190078eec1f5fab3a31d615024492b3c3
FrostSource <jjhfrost@gmail.com>
2024-09-12 20:48:35 +1200

    Delete SIS init

--- scripts/vscripts/2182586257.lua
+++
@@ -1,4 +0,0 @@
--- Some addons search the vscripts root for workshop IDs, but overwrite core/coreinit.lua, thus preventing Scalable Init Support from initializing if it doesn't have priority
--- This file allows Scalable Init Support to successfully initialize in such a context; using require() ensures the file is only called once
-
-require("core/scalableinit")
