ac160b1a45e58bb5250c6eb56f42f7243110650c
FrostSource <jjhfrost@gmail.com>
2025-07-11 17:31:12 +1200

    Add vis to alyxlib_extras tab

--- scripts/vscripts/alyxlib/debug/debug_menu_extras.lua
+++ scripts/vscripts/alyxlib/debug/debug_menu_extras.lua
@@ -9,17 +9,21 @@ local version = "v1.0.0"
 
 require "alyxlib.debug.debug_menu"
 
-local id = "alyxlib_extras"
+local categoryId = "alyxlib_extras"
 
-DebugMenu:AddCategory(id, "AlyxLib Extras")
+DebugMenu:AddCategory(categoryId, "AlyxLib Extras")
 
-DebugMenu:AddToggle(id, id.."_showtriggers", "Show Triggers", "showtriggers")
+DebugMenu:AddSeparator(categoryId, nil, "Display")
 
-DebugMenu:AddToggle(id, id.."_luxels", "Mat Luxels", "mat_luxels")
+DebugMenu:AddToggle(categoryId, "showtriggers", "Show Triggers", "showtriggers")
 
-DebugMenu:AddToggle(id, id.."_fullbright", "Fullbright", "mat_fullbright")
+DebugMenu:AddToggle(categoryId, "luxels", "Mat Luxels", "mat_luxels")
 
-DebugMenu:AddToggle(id, id.."_visfreeze", "Freeze Vis", function(on)
+DebugMenu:AddToggle(categoryId, "fullbright", "Fullbright", "mat_fullbright")
+
+DebugMenu:AddToggle(categoryId, "visibility", "Vis", "vis_enable")
+
+DebugMenu:AddToggle(categoryId, categoryId.."_visfreeze", "Freeze Vis", function(on)
     if on then
         SendToConsole("vis_debug_show 1")
         Player:Delay(function()
