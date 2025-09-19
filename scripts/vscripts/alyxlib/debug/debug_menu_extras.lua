80293e8f912e4d1f14be603a66552811c64105b7
FrostSource <jjhfrost@gmail.com>
2025-09-20 00:01:21 +1200

    add r_cubemap_debug_colors to extras

--- scripts/vscripts/alyxlib/debug/debug_menu_extras.lua
+++ scripts/vscripts/alyxlib/debug/debug_menu_extras.lua
@@ -23,6 +23,8 @@ DebugMenu:AddToggle(categoryId, "fullbright", "Fullbright", "mat_fullbright")
 
 DebugMenu:AddToggle(categoryId, "visibility", "Vis", "vis_enable")
 
+DebugMenu:AddToggle(categoryId, "cubemapcolors", "Cubemap Colors", "r_cubemap_debug_colors")
+
 DebugMenu:AddToggle(categoryId, categoryId.."_visfreeze", "Freeze Vis", nil, function(on)
     if on then
         SendToConsole("vis_debug_show 1")
